0c8624128ccf7d6b99c85a0ee6b3ecc7554948a1
CSW <772862194@qq.com>
2020-08-21 22:51:50 +0800

    测试修改以及新增忽略列表

--- CN2Pic.cpp
+++ CN2Pic.cpp
@@ -12,17 +12,6 @@ using namespace cv;
 static String ext = ".png";//使用png格式图片，无损压缩
 static String path = "./Resources/mine/";//文件路径
 
-/*#include <windows.h>
-char* wchar2char(const wchar_t* wchar)
-{
-	char* m_char;
-	int len = WideCharToMultiByte(CP_ACP, 0, wchar, wcslen(wchar), NULL, 0, NULL, NULL);
-	m_char = new char[len + 1];
-	WideCharToMultiByte(CP_ACP, 0, wchar, wcslen(wchar), m_char, len, NULL, NULL);
-	m_char[len] = '\0';
-	return m_char;
-}*/
-
 /*int main() {
 	vector<Mat> channels;
 	cv::split(srcImg, channels);
